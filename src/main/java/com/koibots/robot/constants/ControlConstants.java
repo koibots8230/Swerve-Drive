959159798379ef406a9dda2ee6b6e87c02abe96f
Caleb O'Neal <caleboneal07@gmail.com>
2023-11-08 19:59:36 -0500

    Ci #8 (#11)
    * Maybe fixed CI
    
    * Allowed CI to be run manually
    
    * Renamed action
    
    * Pretty sure this should work
    
    * Pretty sure this should work
    
    * Nevermind
    
    * Maybe this?
    
    * Fixed dumb stuff
    
    * Maybe this time
    
    * Added basic BuildConstants.java file
    
    * Removed non-ASCII character that github didn't like
    
    * Updated gitignore
    
    * Formatting stuff
    
    * Fixed BuildConstants location
    
    * Fixed BuildConstants identation and added elastic config

--- src/main/java/com/koibots/robot/constants/ControlConstants.java
+++ src/main/java/com/koibots/robot/constants/ControlConstants.java
@@ -6,15 +6,16 @@ import edu.wpi.first.math.util.Units;
 
 public class ControlConstants {
 
-    public static final double DEADBAND = 0.02;
+  public static final double DEADBAND = 0.02;
 
-    private static final double ROBOT_WIDTH_METERS = Units.inchesToMeters(30);
-    private static final double ROBOT_LENGTH_METERS = Units.inchesToMeters(30);
+  private static final double ROBOT_WIDTH_METERS = Units.inchesToMeters(30);
+  private static final double ROBOT_LENGTH_METERS = Units.inchesToMeters(30);
 
-    public static final SwerveDriveKinematics SWERVE_KINEMATICS = new SwerveDriveKinematics(
-            new Translation2d(ROBOT_LENGTH_METERS / 2, ROBOT_WIDTH_METERS / 2), // Front Left
-            new Translation2d(ROBOT_LENGTH_METERS / 2, -ROBOT_WIDTH_METERS / 2), // Front Right
-            new Translation2d(-ROBOT_LENGTH_METERS / 2, ROBOT_WIDTH_METERS / 2), // Back Left
-            new Translation2d(-ROBOT_LENGTH_METERS / 2, -ROBOT_WIDTH_METERS / 2)  // Front Right
-    );
+  public static final SwerveDriveKinematics SWERVE_KINEMATICS =
+      new SwerveDriveKinematics(
+          new Translation2d(ROBOT_LENGTH_METERS / 2, ROBOT_WIDTH_METERS / 2), // Front Left
+          new Translation2d(ROBOT_LENGTH_METERS / 2, -ROBOT_WIDTH_METERS / 2), // Front Right
+          new Translation2d(-ROBOT_LENGTH_METERS / 2, ROBOT_WIDTH_METERS / 2), // Back Left
+          new Translation2d(-ROBOT_LENGTH_METERS / 2, -ROBOT_WIDTH_METERS / 2) // Front Right
+          );
 }
