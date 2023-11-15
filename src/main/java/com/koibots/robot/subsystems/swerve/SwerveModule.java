be0ee849ff4916b8935b9e579190859ea1cdde36
CalebNeal07 <caleboneal07@gmail.com>
2023-11-14 21:30:59 -0500

    Fixed broken stufff

--- src/main/java/com/koibots/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/com/koibots/robot/subsystems/swerve/SwerveModule.java
@@ -71,12 +71,6 @@ public class SwerveModule {
     io.updateInputs(inputs);
     Logger.processInputs("Drive/Module" + Integer.toString(index), inputs);
 
-    // On first cycle, reset relative turn encoder
-    // Wait until absolute angle is nonzero in case it wasn't initialized yet
-    if (turnRelativeOffset == null && inputs.turnAbsolutePosition.getRadians() != 0.0) {
-      turnRelativeOffset = inputs.turnAbsolutePosition.minus(inputs.turnPosition);
-    }
-
     // Run closed loop turn control
     if (angleSetpoint != null) {
       io.setTurnVoltage(
@@ -135,7 +129,7 @@ public class SwerveModule {
     if (turnRelativeOffset == null) {
       return new Rotation2d();
     } else {
-      return inputs.turnPosition.plus(turnRelativeOffset);
+      return inputs.turnAbsolutePosition.plus(turnRelativeOffset);
     }
   }
 
