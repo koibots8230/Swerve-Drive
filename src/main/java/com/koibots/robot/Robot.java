a809f1b36391d117eb43874b09e6d0922bd5b64b
CalebNeal07 <caleboneal07@gmail.com>
2023-11-15 09:15:41 -0500

    Added some gradle stuff, updated AdvantageKit, and renamed SwerveCommand

--- src/main/java/com/koibots/robot/Robot.java
+++ src/main/java/com/koibots/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import lombok.Getter;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.inputs.LoggedPowerDistribution;
@@ -26,12 +27,7 @@ public class Robot extends LoggedRobot {
         REPLAY
     }
 
-    private static final Mode robotMode = isReal() ? Mode.REAL : Constants.SET_REPLAY ? Mode.REPLAY : Mode.SIM;
-
-    public static Mode getMode() {
-        return robotMode;
-    }
-
+    @Getter private static final Mode mode = isReal() ? Mode.REAL : Constants.SET_REPLAY ? Mode.REPLAY : Mode.SIM;
     /**
      * This function is run when the robot is first started up and should be used
      * for any
@@ -55,7 +51,7 @@ public class Robot extends LoggedRobot {
         // Instantiate our RobotContainer.
         // This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
-        robotContainer = new RobotContainer(robotMode);
+        robotContainer = new RobotContainer(mode);
     }
 
     /**
--- src/main/java/com/koibots/robot/subsystems/Subsystems.java
+++ src/main/java/com/koibots/robot/subsystems/Subsystems.java
@@ -3,6 +3,11 @@ package com.koibots.robot.subsystems;
 import com.koibots.robot.subsystems.swerve.Swerve;
 import java.util.function.Supplier;
 
+/**
+ * Used to manage subsystem initialization
+ * 
+ * @author Caleb O'Neal
+ */
 public class Subsystems {
     private static Swerve swerveInstance;
     public static Supplier<Swerve> Swerve;
