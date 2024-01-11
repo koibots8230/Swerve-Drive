b868f8e32c6022c519b35ff45465aa5c54a8bd8d
JakeStew81 <jakestewart711@icloud.com>
2024-01-10 16:44:39 -0800

    Fixed wpilib 2024 stuff

---
+++ src/main/java/com/koibots/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.koibots.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "Swerve-Drive";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 27;
+    public static final String GIT_SHA = "8f6804f635499e6d9d9e75eb65b99cdd801881ce";
+    public static final String GIT_DATE = "2023-12-13 15:25:10 EST";
+    public static final String GIT_BRANCH = "dev";
+    public static final String BUILD_DATE = "2023-12-13 18:52:28 EST";
+    public static final long BUILD_UNIX_TIME = 1702511548061L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants(){}
+}
